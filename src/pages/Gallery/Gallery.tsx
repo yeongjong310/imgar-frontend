4b62d8091db2c8f9fcc1642792d56896cd9b28a9
yeongjong310 <tohero21@naver.com>
2021-11-07 20:18:09 +0900

    feat: test 를 위해 profiler 추가

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState, Profiler } from 'react';
 
 // components
 import {
@@ -61,57 +61,72 @@ export default function Gallery(): ReactElement {
   }, [isLoading]);
 
   return (
-    !isLoading && (
-      <MainContainer
-        sticky
-        customHeaderHeight={65}
-        customHeader={
-          <GalleryHeader
-            positionX={postHeaderTitlePosX}
-            width={postHeaderTitleWidth}
-            title={data.title}
-            username={data.accountName}
-          />
-        }
+    <>
+      <Profiler
+        id="gallery"
+        onRender={(id, phase, actualTime, baseTime, startTime, commitTime, interactions) => {
+          console.log('id', id);
+          console.log('phase', phase);
+          console.log('actualTime', actualTime);
+          console.log('baseTime', baseTime);
+          console.log('startTime', startTime);
+          console.log('commitTime', commitTime);
+          console.log('interactions', interactions);
+        }}
       >
-        <PostContainer>
-          <PostHeader
-            className="header"
-            ref={postHeaderRef}
-            username={data.accountName}
-            title={data.title}
-            metaInfos={{ time: data.dateTime, views: data.views, platform: 'Iphone' }}
-          />
-          <PostContents className="contents" images={data.images} tags={data.tags} />
+        {!isLoading && (
+          <MainContainer
+            sticky
+            customHeaderHeight={65}
+            customHeader={
+              <GalleryHeader
+                positionX={postHeaderTitlePosX}
+                width={postHeaderTitleWidth}
+                title={data.title}
+                username={data.accountName}
+              />
+            }
+          >
+            <PostContainer>
+              <PostHeader
+                className="header"
+                ref={postHeaderRef}
+                username={data.accountName}
+                title={data.title}
+                metaInfos={{ time: data.dateTime, views: data.views, platform: 'Iphone' }}
+              />
+              <PostContents className="contents" images={data.images} tags={data.tags} />
 
-          <PostSideVoteBar className="left-side" votePoints={data.points} commentCount={data.commentCount} />
-          <PostComments className="comments" commentCount={data.commentCount} postId={param.id} />
-          <div className="right-side">
-            <PostSideRelativeList className="relative-list" mainPostId={param.id}></PostSideRelativeList>
-          </div>
-        </PostContainer>
-        <ExplorePostsContainer $width={masonryWidth}>
-          <h3>
-            <Button
-              css={`
-                width: ${pxToRem(400)};
-              `}
-              className="explore-btn"
-              size="large"
-              backgroundColor="darkGray"
-              hoverBackgroundColor="secondaryColor"
-              text={isOpenMasonry ? 'UNEXPLORE POSTS' : 'EXPLORE POSTS'}
-              onClick={() => {
-                setIsOpenMasonry(!isOpenMasonry);
-              }}
-              aria-haspopup="true"
-              aria-owns="masonry"
-              aria-expanded={isOpenMasonry}
-            />
-          </h3>
-          {isOpenMasonry && <MasonryGalleryContainer id="masonry" />}
-        </ExplorePostsContainer>
-      </MainContainer>
-    )
+              <PostSideVoteBar className="left-side" votePoints={data.points} commentCount={data.commentCount} />
+              <PostComments className="comments" commentCount={data.commentCount} postId={param.id} />
+              <div className="right-side">
+                <PostSideRelativeList className="relative-list" mainPostId={param.id}></PostSideRelativeList>
+              </div>
+            </PostContainer>
+            <ExplorePostsContainer $width={masonryWidth}>
+              <h3>
+                <Button
+                  css={`
+                    width: ${pxToRem(400)};
+                  `}
+                  className="explore-btn"
+                  size="large"
+                  backgroundColor="darkGray"
+                  hoverBackgroundColor="secondaryColor"
+                  text={isOpenMasonry ? 'UNEXPLORE POSTS' : 'EXPLORE POSTS'}
+                  onClick={() => {
+                    setIsOpenMasonry(!isOpenMasonry);
+                  }}
+                  aria-haspopup="true"
+                  aria-owns="masonry"
+                  aria-expanded={isOpenMasonry}
+                />
+              </h3>
+              {isOpenMasonry && <MasonryGalleryContainer id="masonry" />}
+            </ExplorePostsContainer>
+          </MainContainer>
+        )}
+      </Profiler>
+    </>
   );
 }
